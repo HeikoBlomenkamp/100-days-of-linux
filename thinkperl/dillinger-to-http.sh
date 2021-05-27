4d569e595b732a5a9041a9b61b970393fb8cd562
Heiko Blomenkamp <heiko.blomenkamp@gmx.de>
2021-05-27 14:24:19 +0200

    modified:   thinkperl/dillinger-to-http.sh (R1D5).

--- thinkperl/dillinger-to-http.sh
+++ thinkperl/dillinger-to-http.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-# $Id: dillinger-to-http.sh,v 1.1 2021/05/24 18:28:57 heiko Exp $
+# $Id: dillinger-to-http.sh,v 1.2 2021/05/27 12:19:48 heiko Exp $
 #
 # $Log: dillinger-to-http.sh,v $
+# Revision 1.2  2021/05/27 12:19:48  heiko
+# R1D5: Added Section 1.2 (What is a program?).
+#
 # Revision 1.1  2021/05/24 18:28:57  heiko
 # Initial revision
 #
@@ -17,7 +20,8 @@ fi
 # Array of all headings
 HEADINGS=('Preface' \
 '1_The_way_of_the_program' \
-'11_The_Perl_programming_language')
+'11_The_Perl_programming_language' \
+'12_What_is_a_program')
 
 # Keep original file
 cp -ua $1 $2
