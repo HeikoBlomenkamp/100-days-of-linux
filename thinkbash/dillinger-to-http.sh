d564b5adac2479fa16c2a76e176d3e7b803f8111
Heiko Blomenkamp <heiko.blomenkamp@gmx.de>
2021-05-27 16:42:52 +0200

    modified:   thinkbash/dillinger-to-http.sh (R1D5).

--- thinkbash/dillinger-to-http.sh
+++ thinkbash/dillinger-to-http.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-# $Id: dillinger-to-http.sh,v 1.1 2021/05/25 12:03:41 heiko Exp $
+# $Id: dillinger-to-http.sh,v 1.2 2021/05/27 13:12:52 heiko Exp $
 #
 # $Log: dillinger-to-http.sh,v $
+# Revision 1.2  2021/05/27 13:12:52  heiko
+# R1D5: Added Section 1.1 (What is a program?).
+#
 # Revision 1.1  2021/05/25 12:03:41  heiko
 # Initial revision
 #
@@ -17,7 +20,8 @@ fi
 # Array of all headings
 HEADINGS=('Preface' \
 '1_The_way_of_the_program' \
-'11_The_Bash_programming_language')
+'11_The_Bash_programming_language' \
+'12_What_is_a_program')
 
 # Keep original file
 cp -ua $1 $2
