bed4166eb7f51dd374c1904cd22900e25e6bf2fb
Heiko Blomenkamp <heiko.blomenkamp@gmx.de>
2021-05-28 23:34:14 +0200

    new file:   thinkperl/main/s131_syntax_errors.pl (R1D6).

---
+++ thinkperl/main/s131_syntax_errors.pl
@@ -0,0 +1,10 @@
+print ((4 + 5) * 10);
+print "\n";
+# 90
+
+# print 8);
+#
+# My error message (if executed):
+#
+# syntax error at s131_syntax_errors.pl line 5, near "8)"
+# Execution of s131_syntax_errors.pl aborted due to compilation errors.
