7169093e96adca4cfd1fa0668ce92513bfb97688
Heiko Blomenkamp <heiko.blomenkamp@gmx.de>
2021-05-28 14:42:09 +0200

    new file:   thinkpython2/main/s131_syntax_errors.py (R1D6).

---
+++ thinkpython2/main/s131_syntax_errors.py
@@ -0,0 +1,11 @@
+print (4 + 5) * 10
+# 90
+
+# print 8)
+#
+# My error message (if executed):
+#
+#  File "s131_syntax_errors.py", line 2
+#     print 8)
+#            ^
+# SyntaxError: invalid syntax
