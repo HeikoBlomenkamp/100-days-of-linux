ef1e869813c432c089efe7c82e07a622688b9e8b
Heiko Blomenkamp <heiko.blomenkamp@gmx.de>
2021-05-28 23:32:07 +0200

    modified:   thinkperl/dillinger-to-http.sh (R1D6).

--- thinkperl/dillinger-to-http.sh
+++ thinkperl/dillinger-to-http.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-# $Id: dillinger-to-http.sh,v 1.2 2021/05/27 12:19:48 heiko Exp $
+# $Id: dillinger-to-http.sh,v 1.3 2021/05/28 21:29:36 heiko Exp $
 #
 # $Log: dillinger-to-http.sh,v $
+# Revision 1.3  2021/05/28 21:29:36  heiko
+# R1D6: Added Section 1.3 (What is debugging?).
+#
 # Revision 1.2  2021/05/27 12:19:48  heiko
 # R1D5: Added Section 1.2 (What is a program?).
 #
@@ -21,7 +24,8 @@ fi
 HEADINGS=('Preface' \
 '1_The_way_of_the_program' \
 '11_The_Perl_programming_language' \
-'12_What_is_a_program')
+'12_What_is_a_program' \
+'13_What_is_debugging')
 
 # Keep original file
 cp -ua $1 $2
