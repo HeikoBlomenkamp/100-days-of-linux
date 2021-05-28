7b2725fb0b69c79638fa77f6b14c43d4816b9a9b
Heiko Blomenkamp <heiko.blomenkamp@gmx.de>
2021-05-29 01:00:37 +0200

    new file:   thinkbash/main/s0131_syntax_errors.sh (R1D6).

---
+++ thinkbash/main/s0131_syntax_errors.sh
@@ -0,0 +1,9 @@
+echo $(( (4 + 5) * 10 ))
+# 90
+
+# echo $(( 8) ))
+#
+# My error message (if executed):
+#
+# s131_syntax_errors.sh: line 4: syntax error near unexpected token `)'
+# s131_syntax_errors.sh: line 4: `echo $(( 8) ))'
