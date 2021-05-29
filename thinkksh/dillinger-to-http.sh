4a7b46f9f5635dc967916e39983a6acb4a6070c1
Heiko Blomenkamp <heiko.blomenkamp@gmx.de>
2021-05-30 00:47:34 +0200

    new file:   thinkksh/dillinger-to-http.sh (R1D7).

---
+++ thinkksh/dillinger-to-http.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# $Id: dillinger-to-http.sh,v 1.1 2021/05/29 22:25:27 heiko Exp $
+#
+# $Log: dillinger-to-http.sh,v $
+# Revision 1.1  2021/05/29 22:25:27  heiko
+# Initial revision
+#
+
+# Print short help message
+if [ -z $2 ]
+then
+	echo "Usage: $0 <source> <target>"
+	exit 2
+fi
+
+# Array of all headings
+HEADINGS=('Preface' \
+'1_The_way_of_the_program' \
+'11_The_KSH_programming_language' \
+'12_What_is_a_program' \
+'13_What_is_debugging')
+
+# Keep original file
+cp -ua $1 $2
+
+# Remove line number from all anchor related phrases
+for heading in "${HEADINGS[@]}"
+do
+	sed -i -e "s/${heading}_[0-9]\+/${heading}/g" $2
+	if [ $? -eq 0 ]
+	then
+		echo "[  OK  ] $heading"
+	else
+		echo "[FAILED] $heading"
+		exit 2
+	fi
+done
+
+exit 0
