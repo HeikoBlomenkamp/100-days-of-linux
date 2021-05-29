d9458c4104763fbc82e0e6df32e47c0f5f1fd798
Heiko Blomenkamp <heiko.blomenkamp@gmx.de>
2021-05-30 00:42:29 +0200

    modified:   thinklua/main/s0131_syntax_errors.lua (R1D7).

--- thinklua/main/s0131_syntax_errors.lua
+++ thinklua/main/s0131_syntax_errors.lua
@@ -4,4 +4,4 @@ print((4 + 5) * 10)
 -- print(8))
 --
 -- My error message (if executed):
--- lua: s131_syntax_errors.lua:4: unexpected symbol near ')'
+-- lua: s0131_syntax_errors.lua:4: unexpected symbol near ')'
