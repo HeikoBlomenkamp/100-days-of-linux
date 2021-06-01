1bd141e0ef033a9c1aa03a0b72e9de9ab50ac7a0
Heiko Blomenkamp <heiko.blomenkamp@gmx.de>
2021-06-01 09:21:29 +0200

    modified:   thinkpython2/dillinger-to-http.sh (R1D10).

--- thinkpython2/dillinger-to-http.sh
+++ thinkpython2/dillinger-to-http.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-# $Id: dillinger-to-http.sh,v 1.3 2021/05/28 12:20:47 heiko Exp $
+# $Id: dillinger-to-http.sh,v 1.4 2021/06/01 07:17:52 heiko Exp $
 #
 # $Log: dillinger-to-http.sh,v $
+# Revision 1.4  2021/06/01 07:17:52  heiko
+# R1D10: Added Section 1.4 (Formal and natural languages).
+#
 # Revision 1.3  2021/05/28 12:20:47  heiko
 # R1D6: Added Section 1.3 (What is debugging?).
 #
@@ -25,7 +28,9 @@ HEADINGS=('Preface' \
 '1_The_way_of_the_program' \
 '11_The_Python_programming_language' \
 '12_What_is_a_program' \
-'13_What_is_debugging')
+'13_What_is_debugging' \
+'14_Formal_and_natural_languages')
+
 
 # Keep original file
 cp -ua $1 $2
