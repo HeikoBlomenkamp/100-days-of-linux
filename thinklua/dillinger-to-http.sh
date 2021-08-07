14bcd2814dbc7f6a2dc5de0bccf46287e2422d91
Heiko Blomenkamp <heiko.blomenkamp@gmx.de>
2021-08-07 05:24:46 +0200

    modified:   thinklua/dillinger-to-http.sh (R1D76).

--- thinklua/dillinger-to-http.sh
+++ thinklua/dillinger-to-http.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-# $Id: dillinger-to-http.sh,v 1.2 2021/05/28 21:53:06 heiko Exp $
+# $Id: dillinger-to-http.sh,v 1.3 2021/06/23 00:01:39 heiko Exp $
 #
 # $Log: dillinger-to-http.sh,v $
+# Revision 1.3  2021/06/23 00:01:39  heiko
+# R1D31: Added Section 1.4 (Formal and natural languages).
+#
 # Revision 1.2  2021/05/28 21:53:06  heiko
 # R1D6: Added Section 1.3 (What is debugging?).
 #
@@ -22,7 +25,8 @@ HEADINGS=('Preface' \
 '1_The_way_of_the_program' \
 '11_The_Lua_programming_language' \
 '12_What_is_a_program' \
-'13_What_is_debugging')
+'13_What_is_debugging' \
+'14_Formal_and_natural_languages')
 
 # Keep original file
 cp -ua $1 $2
