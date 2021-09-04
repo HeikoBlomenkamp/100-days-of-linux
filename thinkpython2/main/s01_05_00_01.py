d9371240a1f4a8760f903215e7db12caf97ac6d9
Heiko Blomenkamp <heiko.blomenkamp@gmx.de>
2021-09-04 17:42:23 +0200

    new file:   thinkpython2/main/s01_05_00_01.py (R2D5).

---
+++ thinkpython2/main/s01_05_00_01.py
@@ -0,0 +1,18 @@
+# $Id: s01_05_00_01.py,v 2.1 2021/09/04 15:34:09 heiko Exp $
+#
+# $Log: s01_05_00_01.py,v $
+# Revision 2.1  2021/09/04 15:34:09  heiko
+# R2D5: The first program.
+#
+
+
+print 'Hello, World!'
+
+# "This is an example of a **print statement**,
+# which doesn't actually print anything on paper.
+# It displays a value on the screen.
+# In this case, the result is the words"
+#
+#####################
+### Hello, World! ###
+#####################
