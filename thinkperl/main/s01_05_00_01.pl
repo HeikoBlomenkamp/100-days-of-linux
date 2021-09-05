883b33a649eac72f59b1bb5b6eec7e07750c8bb1
Heiko Blomenkamp <heiko.blomenkamp@gmx.de>
2021-09-05 22:21:49 +0200

    new file:   thinkperl/main/s01_05_00_01.pl (R2D6).

---
+++ thinkperl/main/s01_05_00_01.pl
@@ -0,0 +1,18 @@
+# $Id: s01_05_00_01.pl,v 2.1 2021/09/05 19:02:15 heiko Exp $
+#
+# $Log: s01_05_00_01.pl,v $
+# Revision 2.1  2021/09/05 19:02:15  heiko
+# R2D6: The first program.
+#
+
+
+print "Hello, World!\n"
+
+# "This is an example of a **print statement**,
+# which doesn't actually print anything on paper.
+# It displays a value on the screen.
+# In this case, the result is the words"
+#
+#####################
+### Hello, World! ###
+#####################
