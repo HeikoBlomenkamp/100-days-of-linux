064ae9869efc2481c80ce37499fe22d507d15868
Heiko Blomenkamp <heiko.blomenkamp@gmx.de>
2021-09-06 16:11:23 +0200

    new file:   thinklua/main/s01_05_00_01.lua (R2D7).

---
+++ thinklua/main/s01_05_00_01.lua
@@ -0,0 +1,18 @@
+-- $Id: s01_05_00_01.lua,v 2.1 2021/09/06 13:16:17 heiko Exp $
+--
+-- $Log: s01_05_00_01.lua,v $
+-- Revision 2.1  2021/09/06 13:16:17  heiko
+-- R2D7: The first program.
+--
+
+
+print 'Hello, World!'
+
+-- "This is an example of a **print statement**,
+-- which doesn't actually print anything on paper.
+-- It displays a value on the screen.
+-- In this case, the result is the words"
+--
+---------------------------
+------ Hello, World! ------
+---------------------------
