98f525cf445468717e303f3b04318ab5d46a0174
Heiko Blomenkamp <heiko.blomenkamp@gmx.de>
2021-09-08 20:16:32 +0200

    new file:   thinkbash/main/s01_05_00_01.sh (R2D9).

---
+++ thinkbash/main/s01_05_00_01.sh
@@ -0,0 +1,8 @@
+# $Id: s01_05_00_01.sh,v 2.1 2021/09/08 17:03:27 heiko Exp $
+#
+# $Log: s01_05_00_01.sh,v $
+# Revision 2.1  2021/09/08 17:03:27  heiko
+# R2D9: The first program.
+#
+
+echo 'Hello, World!'
