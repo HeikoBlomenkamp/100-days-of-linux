f3f9bcdf4857249f27564a93c780e0b710c56e99
Heiko Blomenkamp <heiko.blomenkamp@gmx.de>
2021-09-08 19:11:09 +0200

    deleted:    thinkbash/main/dinsdale.sh (R2D9).
    deleted:    thinkbash/main/s0131_syntax_errors.sh

--- thinkbash/main/s0131_syntax_errors.sh
+++
@@ -1,9 +0,0 @@
-echo $(( (4 + 5) * 10 ))
-# 90
-
-# echo $(( 8) ))
-#
-# My error message (if executed):
-#
-# s0131_syntax_errors.sh: line 4: syntax error near unexpected token `)'
-# s0131_syntax_errors.sh: line 4: `echo $(( 8) ))'
