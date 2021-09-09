6909d8deb0a285e6b63be9f47fadf6958b768f7a
Heiko Blomenkamp <heiko.blomenkamp@gmx.de>
2021-09-09 15:56:26 +0200

    modified:   thinkpython2/main/s01_05_00_01.py (R2D10).

--- thinkpython2/main/s01_05_00_01.py
+++ thinkpython2/main/s01_05_00_01.py
@@ -1,18 +1,11 @@
-# $Id: s01_05_00_01.py,v 2.1 2021/09/04 15:34:09 heiko Exp $
+# $Id: s01_05_00_01.py,v 2.2 2021/09/09 12:30:08 heiko Exp $
 #
 # $Log: s01_05_00_01.py,v $
+# Revision 2.2  2021/09/09 12:30:08  heiko
+# R2D10: Deleted the print statement comment.
+#
 # Revision 2.1  2021/09/04 15:34:09  heiko
 # R2D5: The first program.
 #
 
-
 print 'Hello, World!'
-
-# "This is an example of a **print statement**,
-# which doesn't actually print anything on paper.
-# It displays a value on the screen.
-# In this case, the result is the words"
-#
-#####################
-### Hello, World! ###
-#####################
