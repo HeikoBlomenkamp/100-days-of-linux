e4c579d912b8e1580cc09469e8cf5459fa510fd4
Heiko Blomenkamp <heiko.blomenkamp@gmx.de>
2021-09-10 18:15:26 +0200

    modified:   thinkperl/main/s01_05_00_01.pl (R2D11).

--- thinkperl/main/s01_05_00_01.pl
+++ thinkperl/main/s01_05_00_01.pl
@@ -1,18 +1,11 @@
-# $Id: s01_05_00_01.pl,v 2.1 2021/09/05 19:02:15 heiko Exp $
+# $Id: s01_05_00_01.pl,v 2.2 2021/09/10 16:00:36 heiko Exp $
 #
 # $Log: s01_05_00_01.pl,v $
+# Revision 2.2  2021/09/10 16:00:36  heiko
+# R2D11: Deleted the print statement comment.
+#
 # Revision 2.1  2021/09/05 19:02:15  heiko
 # R2D6: The first program.
 #
 
-
 print "Hello, World!\n"
-
-# "This is an example of a **print statement**,
-# which doesn't actually print anything on paper.
-# It displays a value on the screen.
-# In this case, the result is the words"
-#
-#####################
-### Hello, World! ###
-#####################
